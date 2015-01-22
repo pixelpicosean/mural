f24f35c94d39d979dbd0fd8487378d804658a21f
Sean <pixelpicosean@gmail.com>
2015-01-22 22:08:04 +0800

    Tests for assets async loading

--- tests/AssetManager.cc
+++ tests/AssetManager.cc
@@ -1,4 +1,5 @@
 #include "../src/MuAssetsManager.h"
+#include "../src/MuSurface.h"
 #include "../src/MuAppController.h"
 
 #include "../src/MuCore.h"
@@ -22,17 +23,50 @@ TYPE loadStringFromFile(const DataSourceRef &dataSource) {
 // Create a controller class for testing
 class GameController : public mural::MuAppController {
     public:
-        GameController(): loaded(false) {}
+        GameController(): fired(false), loaded(false) {}
         ~GameController() {}
-        void update(Number dt) {
-            if (!loaded) {
-                test();
-                loaded = true;
+        void update(Number dt) {}
+        void render(NVGcontext *ctx) {
+            if (!fired) {
+                test(ctx);
+                fired = true;
             }
+
+            // Drawing loaded assets
+            nvgSave(ctx);
+
+                int x = 960 / 2,
+                    y = 640 / 2;
+
+                int w, h;
+                NVGpaint imagePaint;
+
+                nvgImageSize(ctx, img, &w, &h);
+                imagePaint = nvgImagePattern(ctx, x - w / 2, y - h / 2, w, h, 0, img, 1.0f);
+                nvgBeginPath(ctx);
+                nvgRect(ctx, x - w / 2, y - h / 2, w, h);
+                nvgFillPaint(ctx, imagePaint);
+                nvgFill(ctx);
+
+                if (loaded) {
+                    nvgImageSize(ctx, img2, &w, &h);
+                    imagePaint = nvgImagePattern(ctx, x - w / 2, y - h / 2 - 100, w, h, 0, img2, 1.0f);
+                    nvgBeginPath(ctx);
+                    nvgRect(ctx, x - w / 2, y - h / 2 - 100, w, h);
+                    nvgFillPaint(ctx, imagePaint);
+                    nvgFill(ctx);
+
+                    nvgFillColor(ctx, nvgRGBA(255, 255, 255, 255));
+                    nvgFontFace(ctx, "FontB");
+                    nvgFontSize(ctx, 64);
+                    nvgTextAlign(ctx, NVG_ALIGN_MIDDLE | NVG_ALIGN_CENTER);
+                    nvgText(ctx, x, y + 100, "Assets from Kenny", nullptr);
+                }
+
+            nvgRestore(ctx);
         }
-        void render(NVGcontext *ctx) {}
         void handleEvent(mural::Event *evt) {}
-        void test() {
+        void test(NVGcontext *ctx) {
             // Search assets folder
             printf("Path of 'text_utf8.txt' is: %s\n\n", assetsManager.getAssetPath("text_utf8.txt").c_str());
 
@@ -47,9 +81,33 @@ class GameController : public mural::MuAppController {
                 printf("Async load 'text_utf8.txt':\n%s\n", content.c_str());
             });
             printf("This should print before async content loading completed\n");
+
+            // Load a image (sync)
+            img = nvgCreateImage(ctx, "assets/ship.png", NVG_IMAGE_GENERATE_MIPMAPS);
+            int w, h;
+            nvgImageSize(ctx, img, &w, &h);
+            printf("Image(id: %d) loaded, size: [%d, %d]\n", img, w, h);
+            // Load a image (async)
+            assetsManager.loadAsset<Surface>("laser.png", [](DataSourceRef data) -> Surface {
+                return Surface(data);
+            }, [this, ctx](Surface surface) {
+                img2 = nvgCreateImageRGBA(ctx, surface.getWidth(), surface.getHeight(), 0, surface.getData());
+                int w, h;
+                nvgImageSize(ctx, img2, &w, &h);
+                loaded = true;
+                printf("Image(id: %d) loaded, size: [%d, %d]\n", img2, w, h);
+            });
+
+            // Load a font
+            assetsManager.loadAsset<Buffer>("Roboto-Bold.ttf", [](DataSourceRef data) -> Buffer {
+                return Buffer(data);
+            }, [ctx](Buffer data) {
+                nvgCreateFontMem(ctx, "FontB", static_cast<unsigned char *>(data.getData()), data.getDataSize(), 0);
+            });
         }
     private:
-        bool loaded;
+        bool fired, loaded;
+        int img, img2;
 };
 
 int main(int argc, char const *argv[]) {
