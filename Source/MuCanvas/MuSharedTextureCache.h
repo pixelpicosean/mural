d921a3c39b49265922cd687fbe1566282a849aa3
Sean Bohan <pixelpicosean@gmail.com>
2015-09-18 20:48:54 +0800

    Finish SharedTextureCache class.

---
+++ Source/MuCanvas/MuSharedTextureCache.h
@@ -0,0 +1,35 @@
+#ifndef Mural_MuSharedTextureCache
+#define Mural_MuSharedTextureCache
+
+#include <map>
+
+namespace mural {
+
+  class MuTexture;
+  class MuSharedTextureCache {
+    public:
+      std::map<std::string, MuTexture *> getTextures();
+      unsigned char *getPremultiplyTable();
+      unsigned char *getUnPremultiplyTable();
+
+      static MuSharedTextureCache& instance() {
+        static MuSharedTextureCache instance;
+        return instance;
+      }
+
+    private:
+      MuSharedTextureCache() {}
+      ~MuSharedTextureCache();
+      MuSharedTextureCache(MuSharedTextureCache const&) {}
+      void operator=(MuSharedTextureCache const&) {}
+
+      std::map<std::string, MuTexture *> textures;
+      unsigned char *premultiplyTable;
+      unsigned char *unPremultiplyTable;
+  };
+
+}
+
+#define theTextureCache mural::MuSharedTextureCache::instance()
+
+#endif
