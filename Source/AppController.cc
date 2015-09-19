0a1469bd19456dfbdb828019fc7295ee9b7b45ef
Sean Bohan <pixelpicosean@gmail.com>
2015-09-19 18:17:53 +0800

    Try to draw a simple shape. (not work yet :/)

--- Source/AppController.cc
+++ Source/AppController.cc
@@ -1,11 +1,21 @@
 #include "AppController.h"
 #include "MuTimer.h"
 
+#include "MuCanvas/MuCanvas.h"
+#include "MuCanvas/2D/MuCanvasContext2D.h"
+
 namespace mural {
 
   AppController::~AppController() {
     if (screenRenderingContext) {
       screenRenderingContext->finish();
+    }
+
+    if (canvas) {
+      delete canvas;
+    }
+    if (ctx) {
+      delete ctx;
     }
   }
 
@@ -14,8 +24,23 @@ namespace mural {
     this->height = height;
     this->devicePixelRatio = devicePixelRatio;
 
-    theScheduler.scheduleMessage([] {
-      printf("timer\n");
+    canvas = new MuCanvas();
+    ctx = dynamic_cast<MuCanvasContext2D *>(canvas->getContext(kMuCanvasContextMode2D));
+
+    if (ctx) {
+      printf("canvas.context exists\n");
+    }
+
+    theScheduler.scheduleMessage([&] {
+      printf("start to draw a rect\n");
+      ctx->state->lineWidth = 5.0f;
+      ctx->state->strokeColor = { .hex = 0xffffffff };
+
+      ctx->moveTo(10, 10);
+      ctx->lineTo(80, 10);
+      ctx->lineTo(80, 80);
+      ctx->lineTo(10, 80);
+      ctx->stroke();
     }, 1000, false);
   }
 
