49917427d134bc4d4fdff9358e2b7d2316ac0771
Sean Bohan <pixelpicosean@gmail.com>
2015-09-19 10:49:37 +0800

    Finish CanvasPattern class.

---
+++ Source/MuCanvas/2D/MuCanvasPattern.h
@@ -0,0 +1,31 @@
+#ifndef Mural_MuCanvasPattern_h
+#define Mural_MuCanvasPattern_h
+
+#include "MuCanvas/MuTexture.h"
+#include "MuCanvasContext2D.h"
+
+namespace mural {
+
+  enum MuCanvasPatternRepeat {
+    kMuCanvasPatternNoRepeat  = 0,
+    kMuCanvasPatternRepeatX   = 1,
+    kMuCanvasPatternRepeatY   = 2,
+    kMuCanvasPatternRepeat    = 1 | 2
+  };
+
+  class MuCanvasPattern : public MuFillable {
+    public:
+      MuTexture *texture;
+      MuCanvasPatternRepeat repeat;
+
+      MuCanvasPattern(MuTexture *texture, MuCanvasPatternRepeat repeat):
+        texture(texture),
+        repeat(repeat)
+      {}
+
+      ~MuCanvasPattern() {}
+  };
+
+}
+
+#endif
