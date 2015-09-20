09f2aefa62a14a74470c9213ca27300ffedc74fb
Sean Bohan <pixelpicosean@gmail.com>
2015-09-20 08:28:53 +0800

    Add style setting method to CanvasContext2DScreen, and add missing glFinish call.

--- Source/MuCanvas/2D/MuCanvasContext2DScreen.cc
+++ Source/MuCanvas/2D/MuCanvasContext2DScreen.cc
@@ -3,6 +3,69 @@
 
 namespace mural {
 
+  void MuCanvasContext2DScreen::setStyle(MuRect newStyle) {
+    if ((style.size.x ? style.size.x : width) != newStyle.size.x ||
+      (style.size.y ? style.size.y : height) != newStyle.size.y) {
+      // Must resize
+      style = newStyle;
+
+      // Only resize if we already have a viewFrameBuffer. Otherwise the style
+      // will be honored in the 'create' call.
+      if (viewFrameBuffer) {
+        resizeTo(width, height);
+      }
+    }
+    else {
+      // Just reposition
+      style = newStyle;
+    }
+  }
+
+  MuRect MuCanvasContext2DScreen::getFrame() {
+    return MuRect(
+      style.origin.x, style.origin.y,
+      (style.size.x ? style.size.x : width),
+      (style.size.y ? style.size.y : height)
+    );
+  }
+
+  void MuCanvasContext2DScreen::resizeTo(short newWidth, short newHeight) {
+    flushBuffers();
+
+    width = newWidth;
+    height = newHeight;
+
+    MuRect frame = getFrame();
+
+    float contentScale = useRetinaResolution ? app.devicePixelRatio : 1;
+    backingStoreRatio = (frame.size.x / (float)width) * contentScale;
+
+    bufferWidth = frame.size.x * contentScale;
+    bufferHeight = frame.size.y * contentScale;
+
+    printf(
+      "Creating ScreenCanvas (2D): "
+        "size: %dx%d, "
+        "style: %.0fx%.0f, "
+        "retina: %s = %.0fx%.0f, "
+        "msaa: %s",
+      width, height,
+      frame.size.x, frame.size.y,
+      (useRetinaResolution ? "yes" : "no"),
+      frame.size.x * contentScale, frame.size.y * contentScale,
+      (msaaEnabled ? "yes" : "no")
+    );
+
+    // Set up the renderbuffer
+    glBindRenderbuffer(GL_RENDERBUFFER, viewRenderBuffer);
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_RENDERBUFFER, viewRenderBuffer);
+
+    // Flip the screen - OpenGL has the origin in the bottom left corner. We want the top left.
+    upsideDown = true;
+
+    resetFramebuffer();
+  }
+
   MuTexture *MuCanvasContext2DScreen::getTexture() {
     // This context may not be the current one, but it has to be in order for
     // glReadPixels to succeed.
@@ -39,6 +102,10 @@ namespace mural {
         // glContext->presentRenderbuffer(GL_RENDERBUFFER);
     }
     needsPresenting = false;
+  }
+
+  void MuCanvasContext2DScreen::finish() {
+    glFinish();
   }
 
 }
--- Source/MuCanvas/2D/MuCanvasContext2DScreen.h
+++ Source/MuCanvas/2D/MuCanvasContext2DScreen.h
@@ -8,11 +8,22 @@ namespace mural {
   class MuCanvasContext2DScreen : public MuCanvasContext2D {
     public:
       MuTexture *getTexture();
+      void setStyle(MuRect newStyle);
+      MuRect getFrame();
 
-      MuCanvasContext2DScreen(): MuCanvasContext2D(200, 150) {}
+      MuCanvasContext2DScreen():
+        MuCanvasContext2D(200, 150),
+        style(0.0f, 0.0f, 200, 150)
+      {
+      }
+
+      void resizeTo(short newWidth, short newHeight);
 
       void present();
-      void finish() {}
+      void finish();
+
+    private:
+      MuRect style;
   };
 
 }
