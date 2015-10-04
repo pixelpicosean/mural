47dfe90c209fd9d1f4abc43d04f75cd51eaae0cc
Sean Bohan <pixelpicosean@gmail.com>
2015-10-04 17:28:47 +0800

    Uncomment path drawing tests. Fill and stroke works pretty nice now except the line joint and miter.

--- src/ciEjectaApp.cpp
+++ src/ciEjectaApp.cpp
@@ -35,39 +35,36 @@ void ciEjectaApp::update() {
     ctx->setFillColor(ColorAf(0.0f, 1.0f, 1.0f, 1.0f));
     ctx->setStrokeColor(ColorAf(1.0f, 0.0f, 1.0f, 1.0f));
     ctx->state->lineWidth = 8;
-    // ctx->state->lineJoin = mural::kMuLineJoinRound;
+    ctx->state->lineJoin = mural::kMuLineJoinRound;
 
     ctx->rect(100, 100, 100, 100);
+    ctx->stroke();
     ctx->fill();
+
+    ctx->rect(220, 100, 100, 100);
+    ctx->fill();
+    ctx->stroke();
+
+    ctx->fillRect(220, 100, 100, 100);
+
+    ctx->setStrokeColor(ColorAf(1.0f, 1.0f, 0.0f, 1.0f));
+    ctx->state->lineWidth = 4;
+    ctx->strokeRect(160, 220, 100, 100);
+
+    ctx->translate(320, 100);
+    ctx->save();
+    ctx->setStrokeColor(ColorAf(0.8f, 0.3f, 0.0f, 1.0f));
+    ctx->setFillColor(ColorAf(0.0f, 0.3f, 0.8f, 1.0f));
+    ctx->beginPath();
+    ctx->moveTo(20, 0);
+    ctx->lineTo(120, 0);
+    ctx->lineTo(120, 100);
+    ctx->closePath();
+    ctx->stroke();
+    ctx->fill();
+    ctx->restore();
+
     ctx->finish();
-
-//    ctx->flushBuffers();
-//    ctx->unbindVertexBuffer();
-
-//    ctx->stroke();
-//    // ctx->fill();
-//
-//    // ctx->rect(220, 100, 100, 100);
-//    // ctx->fill();
-//    // ctx->stroke();
-//
-//    ctx->fillRect(220, 100, 100, 100);
-//
-//    ctx->setStrokeColor(ColorAf(1.0f, 1.0f, 0.0f, 1.0f));
-//    ctx->state->lineWidth = 4;
-//    ctx->strokeRect(160, 220, 100, 100);
-//
-//    ctx->translate(320, 100);
-//    ctx->save();
-//    ctx->setStrokeColor(ColorAf(0.5f, 10.f, 0.5f, 1.0f));
-//    ctx->beginPath();
-//    ctx->moveTo(20, 0);
-//    ctx->lineTo(120, 0);
-//    ctx->lineTo(120, 100);
-//    ctx->closePath();
-//    ctx->stroke();
-//    // ctx->fill();
-//    ctx->restore();
 
     finished = true;
   }
